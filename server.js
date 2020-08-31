ec0169d6fe46a97b40d980a07619f3d1c9d82479
Gztabo21 <Gustavocacharuco@gmail.com>
2020-08-30 23:21:10 -0400

    cambio del print port

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/public/'));
 
 app.listen('8080', function() {
-  console.log('Servidor web escuchando en el puerto 3000');
+  console.log('Servidor web escuchando en el puerto 8080');
 });
